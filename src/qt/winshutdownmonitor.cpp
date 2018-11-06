d1136fe656bbb03c6aa9f169418f7f1d07eeaec1
Didy Septiyono <didy.imers@gmail.com>
2018-11-06 21:59:11 +0700

    make it better

--- src/qt/winshutdownmonitor.cpp
+++ src/qt/winshutdownmonitor.cpp
@@ -52,7 +52,7 @@ bool WinShutdownMonitor::nativeEventFilter(const QByteArray& eventType, void* pM
 
 void WinShutdownMonitor::registerShutdownBlockReason(const QString& strReason, const HWND& mainWinId)
 {
-    typedef BOOL(WINAPI * PSHUTDOWNBRCREATE)(HWND, DVDWSTR);
+    typedef BOOL(WINAPI * PSHUTDOWNBRCREATE)(HWND, LPCWSTR);
     PSHUTDOWNBRCREATE shutdownBRCreate = (PSHUTDOWNBRCREATE)GetProcAddress(GetModuleHandleA("User32.dll"), "ShutdownBlockReasonCreate");
     if (shutdownBRCreate == NULL) {
         qWarning() << "registerShutdownBlockReason: GetProcAddress for ShutdownBlockReasonCreate failed";
